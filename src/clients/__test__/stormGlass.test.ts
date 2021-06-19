dd03e5b1bff820c427cd03632014badc7e81ccc9
bruno <brunosilva@catho.com>
2021-06-19 18:16:33 -0300

    test(unit-test): [BR] adjust unit test and mock data

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,21 +1,21 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
 import stormGlassMock from '@tests/fixtures/stormglass_weather.json';
-import stormGlassNormalize from '@tests/fixtures/stormglass_normalize.json';
+import stormGlassNormalizeMock from '@tests/fixtures/stormglass_normalize.json';
 
 jest.mock('axios');
 
 describe('Starting Unit Tests', () => {
   describe('StormGlass Client', () => {
     it('should return the normalized forecast from the StormGlass service', async () => {
-      const lat = -33.666333;
-      const lng = 151.666333;
+      const lat = -33.792726;
+      const lng = 151.289824;
 
-      axios.get = jest.fn().mockResolvedValue(stormGlassMock);
+      axios.get = jest.fn().mockResolvedValue({data: stormGlassMock});
 
       const stormGlass = new StormGlass(axios);
       const response = await stormGlass.fetchPoints(lat, lng);
-      expect(response).toEqual(stormGlassNormalize);
+      expect(response).toEqual(stormGlassNormalizeMock);
     });
   });
 });
